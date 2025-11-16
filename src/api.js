554f1984aa49818498d48f63d0dc72d8b67b747e
Acin639 <titusnjeru48@gmail.com>
2025-11-16 22:54:19 +0300

    Hi

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 const BASE = 'https://api.themoviedb.org/3';
-const KEY = process.env.REACT_APP_TMDB_API_KEY;
+const KEY = "f43ec82a5f24fe6190891894b7436c7a";
 
 export async function fetchMovie(id){
   const res = await fetch(`${BASE}/movie/${id}?api_key=${KEY}&language=en-US&append_to_response=credits`);
